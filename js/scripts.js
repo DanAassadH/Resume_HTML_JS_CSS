adb6889db20627b28970a5fc7353e208d80b1c59
Danish <danishriz100@gmail.comgit commitexit>
2020-09-03 15:48:11 -0600

    Contact form working

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,9 @@
-/*<section class="#contact-form">
-<form action="#">
-
-            <input type="text" id="your-name" value=""><br>
-            <input type="text" id="invalid-name" value=""><br>
-
-            <input type="text" id="your-email" placeholder="abc@example.com" value=""><br>
-            <input type="text" id="invalid-email" value=""><br>
-
-            <textarea id="message" value = "">
-            <input type="text" id="invalid-message" value=""><br>
-
-</form>
-*/
 
 class Contact {
-    constructor(yourName, yourEmail, message) {
+    constructor(yourName, yourEmail, subject, message) {
         this.yourName = yourName;
         this.yourEmail = yourEmail;
+        this.subject = subject;
         this.message = message;
     }
 
@@ -28,81 +15,112 @@ class Contact {
         return false;
     }
 
-    /// Function to check valid email  https://www.codespot.org/javascript-email-validation/
-    /// Expression to test email condition is taken from https://www.w3resource.com/javascript/form/email-validation.php 
+    /// Function to check valid email 
+    /// Expression to test email condition is taken from //https://www.codespot.org/javascript-email-validation/
+    //   Additional resource                      https://www.w3resource.com/javascript/form/email-validation.php 
     ValidEmail() {
-
-        //https://www.codespot.org/javascript-email-validation/
         return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.yourEmail);
-
     }
 
     // Function to find bad words in the form
     SwearCheck(userInput) {
         const swearWords = ["feldercarb", "frack", "skinjob", "vulgacarb"];
-        for (let i = 0; i< swearWords.length;i++) {
-            if (userInput.includes(swearWords[i]))
-             {
-                console.log(swearWords[i])
+        for (let i = 0; i < swearWords.length; i++) {
+            if (userInput.includes(swearWords[i])) {
+              //  console.log(swearWords[i])
                 return true;
             }
         }
         return false;
     }
 
+} //end class Contact
 
-} //end class User
-
-
+const contactInfo = new Contact();
+const contactForm = document.querySelector('form');
 
 /////Testing Block///////////////////////////////////////////////////////////////////////
 
+/* const contactInfo = new Contact(
+     "vulgacarb",
+"=f//A##ani@dcx.com",
+""
+); */
 
-const contactInfo = new Contact();
-  /*  "vulgacarb",
-    "=f//A##ani@dcx.com",
-    "I am saying hello"
-);*/
-
-
-/* const x = contactInfo.yourName;
-
-const y = contactInfo.SwearCheck(x);
-
-console.log(y); */
-
-
-
-  ////Functionality on submit//////////////////////////////////////////////////////////////////////
-
+////Functionality on submit//////////////////////////////////////////////////////////////////////
 
 //// function to get values from text fields  
-function getFormValues()
-{
-    const name = document.getElementById('your-name').value;
-    contactInfo.yourName =name;
-
-    const email = document.getElementById('your-email').value;
-    contactInfo.yourEmail =email;
-
-    const message = document.getElementById('message').value;
-    contactInfo.message =message;
-  
-    console.log(contactInfo.ValidEmail());
+function getFormValues() {
+    contactInfo.yourName = document.getElementById('your-name').value.trim();
+    contactInfo.yourEmail = document.getElementById('your-email').value.trim();
+    contactInfo.subject = document.getElementById('subject').value.trim();
+    contactInfo.message = document.getElementById('message').value.trim();
 }
 
+// function to validate all text fields----- return false if there are any errors in form 
 
-// listening for button send
-  const contactForm = document.querySelector( 'form' );
+function ValidateFormValues() {
+    const name = contactInfo.yourName;
+    const email = contactInfo.yourEmail;
+    const subject = contactInfo.subject;
+    const message = contactInfo.message;
+    let counter = 0;
+    let errorArray = [];
+    //  console.log(name + ':' + email + ':' + subject + ':' + message);
 
-  contactForm.addEventListener('submit', (event)=> {
+    if (contactInfo.IsEmpty(name)) {
+        errorArray[counter] = "Please Enetr Your Name";
+        counter++;
+    }
 
+    if (contactInfo.IsEmpty(subject)) {
+        errorArray[counter] = "Please Enter Subject of your Email";
+        counter++;
+    }
+
+    if (contactInfo.IsEmpty(message)) {
+        errorArray[counter] = "Please Enetr a message";
+        counter++;
+    }
+
+    if (!contactInfo.ValidEmail()) {
+        errorArray[counter] = "Please Enetr a valid email";
+        counter++;
+    }
+
+    if (contactInfo.SwearCheck(`${name} ${email} ${subject} ${message}`)) {
+        errorArray[counter] = "Please keep the text free from swear words";
+        counter++;
+    }
+
+    //  console.log(`counter: ${counter} error messages : ${errorArray}`);
+
+    if (counter > 0) {
+        const errorList = document.querySelector('#display-error');
+        errorList.textContent = "";
+
+        for (let i = 0; i < counter; i++) {
+            const newLI = document.createElement('LI');
+            newLI.textContent = errorArray[i];
+            errorList.appendChild(newLI);
+        }
+        return false;
+    }
+    return true;
+} // end ValidateFormValues
+
+
+// listening for "Send email" button click
+
+contactForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    getFormValues();
+    getFormValues();                                     // calling function to get values of form fields
+    const validationCheck = ValidateFormValues();        // apply validation checks on values
 
-   document.location.href = `mailto:dhabib1@ualberta.ca?&subject=Message%20from%20${contactInfo.yourName}&body=${contactInfo.message}`;
-   
+    if (validationCheck) {                              // if the validation checks dont give any error ,, open default mail client
+        document.location.href = `mailto:dhabib1@ualberta.ca?&subject=${contactInfo.subject}&body=${contactInfo.message}%0D%0AMessage%20from%20${contactInfo.yourName}`;
+    }
 
-  }
-    );
+}
+);
