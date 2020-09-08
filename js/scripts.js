7073a5535b61c72b6ca3d026102e74beeacf5b1a
Danish <danishriz100@gmail.comgit commitexit>
2020-09-08 01:23:38 -0600

    Functional- all Validations passed , minus challanges

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 class Contact {
     constructor(yourName, yourEmail, subject, message) {
         this.yourName = yourName;
@@ -7,58 +6,50 @@ class Contact {
         this.message = message;
     }
 
-    // Function to check if input string is empty
+    // Function to check if input string is empty return true if the field is empty
     IsEmpty(checkString = "") {
+        let check = true;
         if (checkString.length == 0) {
-            return true;
+            check = true;
         }
-        return false;
+        else {
+            check = false;
+        }
+        return check;
     }
 
-    /// Function to check valid email 
+    /* Function to check valid email 
     /// Expression to test email condition is taken from //https://www.codespot.org/javascript-email-validation/
     //   Additional resource                      https://www.w3resource.com/javascript/form/email-validation.php 
+        return true if email adress is valid*/
     ValidEmail() {
         return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.yourEmail);
     }
 
-    // Function to find bad words in the form
+    // Function to find bad words in the given string - return true if there are any bad words in text
     SwearCheck(userInput) {
         const swearWords = ["feldercarb", "frack", "skinjob", "vulgacarb"];
         for (let i = 0; i < swearWords.length; i++) {
             if (userInput.includes(swearWords[i])) {
-              //  console.log(swearWords[i])
                 return true;
             }
         }
         return false;
     }
-
 } //end class Contact
 
 const contactInfo = new Contact();
 const contactForm = document.querySelector('form');
 
-/////Testing Block///////////////////////////////////////////////////////////////////////
-
-/* const contactInfo = new Contact(
-     "vulgacarb",
-"=f//A##ani@dcx.com",
-""
-); */
-
-////Functionality on submit//////////////////////////////////////////////////////////////////////
-
-//// function to get values from text fields  
-function getFormValues() {
-    contactInfo.yourName = document.getElementById('your-name').value.trim();
-    contactInfo.yourEmail = document.getElementById('your-email').value.trim();
+// function to get values from text fields and add them to contactInfo class
+function GetFormValues() {
+    contactInfo.yourName = document.getElementById('yourName').value.trim();
+    contactInfo.yourEmail = document.getElementById('yourEmail').value.trim();
     contactInfo.subject = document.getElementById('subject').value.trim();
     contactInfo.message = document.getElementById('message').value.trim();
 }
 
 // function to validate all text fields----- return false if there are any errors in form 
-
 function ValidateFormValues() {
     const name = contactInfo.yourName;
     const email = contactInfo.yourEmail;
@@ -66,7 +57,7 @@ function ValidateFormValues() {
     const message = contactInfo.message;
     let counter = 0;
     let errorArray = [];
-    //  console.log(name + ':' + email + ':' + subject + ':' + message);
+    let check = true;
 
     if (contactInfo.IsEmpty(name)) {
         errorArray[counter] = "Please Enter Your Name";
@@ -88,17 +79,13 @@ function ValidateFormValues() {
         counter++;
     }
 
-
-
     if (contactInfo.SwearCheck(`${name} ${email} ${subject} ${message}`)) {
         errorArray[counter] = "Please keep the text free from swear words";
         counter++;
     }
 
-    //  console.log(`counter: ${counter} error messages : ${errorArray}`);
-
     if (counter > 0) {
-        const errorList = document.querySelector('#display-error');
+        const errorList = document.querySelector('#displayError');
         errorList.textContent = "";
 
         for (let i = 0; i < counter; i++) {
@@ -106,23 +93,25 @@ function ValidateFormValues() {
             newLI.textContent = errorArray[i];
             errorList.appendChild(newLI);
         }
-        return false;
+        check = false;
     }
-    return true;
+    else {
+        const errorList = document.querySelector('#displayError');
+        errorList.textContent = "Thank you for contacting me!";
+        check = true;
+    }
+    return check;
 } // end ValidateFormValues
 
 
 // listening for "Send email" button click
-
 contactForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    getFormValues();                                     // calling function to get values of form fields
+    GetFormValues();                                     // calling function to get values of form fields
     const validationCheck = ValidateFormValues();        // apply validation checks on values
 
     if (validationCheck) {                              // if the validation checks dont give any error ,, open default mail client
         document.location.href = `mailto:dhabib1@ualberta.ca?&subject=${contactInfo.subject}&body=${contactInfo.message}%0D%0AMessage%20from%20${contactInfo.yourName}`;
     }
-
-}
-);
+});
--- readme.md
+++ readme.md
@@ -1,1 +1,33 @@
 Trello : https://trello.com/b/dWYkJyOA/resume-website
+
+Wireframe tool : https://www.figma.com/
+
+Image Sources :
+
+https://pixabay.com/vectors/gymnast-stick-man-man-stick-2353968/
+
+https://pixabay.com/photos/tech-circle-technology-abstract-5090539/
+
+https://www.pexels.com/photo/multi-colored-folders-piled-up-159519/
+
+https://pixabay.com/photos/paper-block-write-note-pen-office-3406864/
+
+Color Pallate generated from image via : https://palettegenerator.com/
+
+Compress Images via : https://tinypng.com/
+
+Font Source : https://www.fontsquirrel.com/fonts/Myndraine
+
+CSS JavaScript and HTML reference websites:
+
+https://css-tricks.com/guides/
+
+https://tympanus.net/codrops/css_reference/
+
+https://www.w3schools.com/
+
+https://www.w3.org/Style/Examples/007/units.en.html
+
+https://www.codebrainer.com/blog/contactForm-in-javascript
+
+
